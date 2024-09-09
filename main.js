f37aea24663e4892d95cc86bb60070de1b696c18
github_user7801 <163008161+nathanremoslinsomanicos@users.noreply.github.com>
2024-09-09 19:32:18 -0300

    j

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 
-function jump() {
+
+const jump = () => {
     mario.classList.add("jump");
 
     setTimeout(() => {
-        mario.classList.remove("jump");
-    },
-        500);
+        mario.classList.remove("jump")
+    } 
+    ,500);
 }
 
     const loop = setInterval (() => {

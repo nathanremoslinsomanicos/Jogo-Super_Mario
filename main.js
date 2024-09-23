bb2c3bd5954643512505b49ce7f6c24089a1d01b
github_user7801 <163008161+nathanremoslinsomanicos@users.noreply.github.com>
2024-09-23 15:53:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const jump = () => {
 }
 
 const updateScore = () => {
-    score += 7;
+    score += 4;
     scoreElement.textContent = score;
     const animationSpeed = 1.5/ (1 + score/ 500);
     pipe.style.animation = `pipe-animation ${animationSpeed}s infinite linear`;

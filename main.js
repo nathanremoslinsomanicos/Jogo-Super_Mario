20f86ff8b823a7bcf3d7feca811ba042267ab883
github_user7801 <163008161+nathanremoslinsomanicos@users.noreply.github.com>
2024-09-24 08:47:29 -0300

    nb

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const gameOverScreen = document.querySelector(".game-over");
 const scoreElement = document.querySelector(".score");
 
 
-audioStart = new Audio("./sound/invencible.mp3");
+audioStart = new Audio("./sound/invencible.mp");
 const gameOverSound = new Audio("./sound/fim_de_jogo.mp3.mp3");
 
 let gameStarted = false;
